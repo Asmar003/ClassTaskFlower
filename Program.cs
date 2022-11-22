07c37848773cf02e052abfa07aef4b39083a21fc
Asmar003 <esmer.ceferova24@gmail.com>
2022-11-22 19:40:23 +0400

    scnd

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassTaskFlower.Models
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Flower nargis = new Flower
+            {
+                Name = "Nargis",
+                Color = "White",
+                Price = 0.5f
+            };
+
+            Flower jasmin = new Flower
+            {
+                Name = "Jasmin",
+                Color = "Purple",
+                Price = 0.7f
+            };
+
+            Bucket bucket = new Bucket();
+            bucket.DiscountPercent = 20;
+
+            bucket.AddFlower(jasmin);
+            bucket.AddFlower(nargis);
+            bucket.AddFlower(jasmin);
+            bucket.AddFlower(nargis);
+        }
+    }
+}
